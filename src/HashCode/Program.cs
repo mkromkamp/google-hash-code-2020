dd231d4d7d75d5a3491739a933866cc2a190583b
James Lappin <lappintheyoung@gmail.com>
2020-02-20 20:05:27 +0000

    High score

--- src/HashCode/Program.cs
+++ src/HashCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -9,17 +10,20 @@ namespace HashCode
         static void Main(string[] args)
         {
             Console.WriteLine("Starting!");
-
+        var scores = new List<long>();
+            
             for (int i = 1; i <= 6; i++)
             {
                 var challenge = Input.Parse(Path.Combine("input", $"sample{i}.in"));
-                var solution = SolutionMartin.Solve(challenge);
+                var solution = SolutionJames.Solve(challenge);
                 Output.Write(solution, $"output/sample{i}.out");
+                
+                scores.Add(solution.Score());
                 
                 Console.WriteLine($"Input {i}: {solution.Score()} points");
             }
             
-            Console.WriteLine("Finished");
+            Console.WriteLine($"Finished with total {scores.Sum()}");
         }
     }
 }
