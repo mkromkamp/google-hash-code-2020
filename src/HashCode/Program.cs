55c991c93d92fe611341ce4f3db56d51bc033840
Martin Kromkamp <m.kromkamp@gmail.com>
2020-02-20 19:02:08 +0000

    Temp

--- src/HashCode/Program.cs
+++ src/HashCode/Program.cs
@@ -7,23 +7,12 @@ namespace HashCode
     {
         static void Main(string[] args)
         {
-            var solution = new Solution();
+            Console.WriteLine("Starting!");
             
-            var lib1 = new SolutionLibrary(1);
-            lib1.Books.AddLast(new Book() {Id = 1});
-            lib1.Books.AddLast(new Book() {Id = 2});
-            solution.Libraries.AddLast(lib1);
-            
-            
-            var lib2 = new SolutionLibrary(2);
-            lib2.Books.AddLast(new Book() {Id = 8});
-            lib2.Books.AddLast(new Book() {Id = 9});
-            solution.Libraries.AddLast(lib2);
-            
+            var challenge = Input.Parse(Path.Combine("input", "sample1.in"));
+            var solution = SolutionMartin.Solve(challenge);
             Output.Write(solution, "output/sample1.out");
             
-            Console.WriteLine("Starting!");
-            Input.Parse(Path.Combine("input", "sample1.in"));
             Console.WriteLine("Finished");
         }
     }
