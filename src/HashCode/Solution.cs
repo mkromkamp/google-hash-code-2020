1b042d2d8aa5dda39d5b9bbc7762d43e774af39b
Martin Kromkamp <m.kromkamp@gmail.com>
2020-02-20 18:19:09 +0000

    Solution impl

--- src/HashCode/Solution.cs
+++ src/HashCode/Solution.cs
@@ -1,7 +1,21 @@
+using System.Collections.Generic;
+
 namespace HashCode
 {
     public class Solution
     {
-        
+        /// <summary>
+        /// Ordered list of the libraries. The first library in the list is signed up first, the last one last.
+        /// </summary>
+        public LinkedList<SolutionLibrary> Libraries { get; set; }
+    }
+
+    public class SolutionLibrary
+    {
+        /// <summary>
+        /// Linked list of books the library is shipping.
+        /// First book is shipped first, last book is shipped last
+        /// </summary>
+        public LinkedList<Book> Books { get; set; }
     }
 }
