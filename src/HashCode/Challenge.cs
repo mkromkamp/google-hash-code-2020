1300c25eb68defcca4f79699e7c2e7596ff9653e
James Lappin <lappintheyoung@gmail.com>
2020-02-20 18:01:46 +0000

    structure

--- src/HashCode/Challenge.cs
+++ src/HashCode/Challenge.cs
@@ -1,7 +1,24 @@
+using System.Collections.Generic;
+
 namespace HashCode
 {
     public class Challenge
     {
+        public int NumberOfDays { get; set; }
+        public int TotalNumberOfBooks { get; set; }
         
+        public List<Library> Type { get; set; }
+    }
+
+    public class Library
+    {
+        public int Id { get; set; }
+        public List<Book> Type { get; set; }
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public int Score { get; set; }
     }
 }
