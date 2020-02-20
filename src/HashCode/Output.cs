babe2928df8e58ad2da2802fb445c4d5c2cf99e8
Martin Kromkamp <m.kromkamp@gmail.com>
2020-02-20 18:33:04 +0000

    output writer, files and example

--- src/HashCode/Output.cs
+++ src/HashCode/Output.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Linq;
 
 namespace HashCode
 {
@@ -10,7 +11,19 @@ namespace HashCode
             {
                 using (var writer = new StreamWriter(outputFile))
                 {
-                    // TODO: use writer to output the solution to the file we have to submit
+                    // First line contains the number of libraries we signed up
+                    writer.WriteLine(solution.Libraries.Count);
+                    
+                    // For each library we create two lines.
+                    foreach (var library in solution.Libraries)
+                    {
+                        // The first line contains the id of the library followed by the number of scanned books
+                        writer.WriteLine($"{library.Id} {library.Books.Count}");
+                        
+                        // The second line contains the order list of book ids
+                        var bookIds = string.Join(" ", library.Books.Select(book => book.Id));
+                        writer.WriteLine(bookIds);
+                    }
                 }
             }
         }
--- src/HashCode/Program.cs
+++ src/HashCode/Program.cs
@@ -7,6 +7,21 @@ namespace HashCode
     {
         static void Main(string[] args)
         {
+            var solution = new Solution();
+            
+            var lib1 = new SolutionLibrary(1);
+            lib1.Books.AddLast(new Book() {Id = 1});
+            lib1.Books.AddLast(new Book() {Id = 2});
+            solution.Libraries.AddLast(lib1);
+            
+            
+            var lib2 = new SolutionLibrary(2);
+            lib2.Books.AddLast(new Book() {Id = 8});
+            lib2.Books.AddLast(new Book() {Id = 9});
+            solution.Libraries.AddLast(lib2);
+            
+            Output.Write(solution, "output/sample1.out");
+            
             Console.WriteLine("Starting!");
             Input.Parse(Path.Combine("input", "sample1.in"));
             
